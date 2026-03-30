0b684143ca7ab8053ba20d70d3c05dd704e028a6
Aishwarya <aishwarya200411@gmail.com>
2026-03-30 12:30:29 +0530

    Add project files.

---
+++ Models/room.cs
@@ -0,0 +1,7 @@
+﻿public class Room
+{
+    public int Id { get; set; }
+    public string RoomType { get; set; }
+    public decimal Price { get; set; }
+    public bool IsAvailable { get; set; } = true;
+}
